b8ca9f7dd5680cace0b788f748305197bede3685
Luke Chinworth <lukechinworth@gmail.com>
2022-08-06 09:27:37 -0500

    rename to solid data summary

--- readme.txt
+++ readme.txt
@@ -1,4 +1,4 @@
-=== WP Data Summary ===
+=== Solid Data Summary ===
 Contributors: soliddigital,lukechinworth
 Tags: post_type, taxonomy, summary
 Stable tag: 1.0.0
@@ -15,7 +15,7 @@ This plugin adds a page in admin which displays a summary of your post and term 
 
 == Installation ==
 
-1. Download, unzip, and upload the plugin folder to `/wp-content/plugins/` directory
+1. Download, unzip, and upload the plugin folder to `wp-content/plugins/` directory
 1. Activate the plugin through the 'Plugins' menu in WordPress
 
 == Changelog ==
--- solid-data-summary.php
+++ solid-data-summary.php
@@ -1,26 +1,26 @@
 <?php
 /**
- * Plugin Name:       WP Data Summary
+ * Plugin Name:       Solid Data Summary
  * Description:       Small plugin that displays a summary of your post types and taxonomies.
  * Version:           1.0.0
  * Author:            Solid Digital
  * Author URI:        https://www.soliddigital.com
  * License:           GPLv2
- * Text Domain:       wp-data-summary
+ * Text Domain:       solid-data-summary
  * Domain Path:       /languages
  */
 
-namespace wp_data_summary;
+namespace solid_data_summary;
 
-add_action('admin_menu', 'wp_data_summary\admin_menu');
+add_action('admin_menu', 'solid_data_summary\admin_menu');
 
 function admin_menu() {
-    add_options_page( 'WP Data Summary', 'WP Data Summary', 'manage_options', 'wp-data-summary', 'wp_data_summary\admin_page' );
+    add_options_page( 'Solid Data Summary', 'Solid Data Summary', 'manage_options', 'solid-data-summary', 'solid_data_summary\admin_page' );
 }
 
 function admin_page() {
     ?>
-    <h1><?php _e('WP Data Summary', 'wp-data-summary') ?></h1>
+    <h1><?php _e('Solid Data Summary', 'solid-data-summary') ?></h1>
     <?php
 
     the_post_types_table();
@@ -47,15 +47,15 @@ function the_post_types_table() {
         return $a['publish'] > $b['publish'] ? -1 : 1;
     });
     ?>
-    <h2><?php _e('Post Types', 'wp-data-summary') ?></h2>
+    <h2><?php _e('Post Types', 'solid-data-summary') ?></h2>
 
     <table>
         <thead>
             <tr>
-                <th><?php _e('Name', 'wp-data-summary') ?></th>
-                <th><?php _e('Label', 'wp-data-summary') ?></th>
-                <th><?php _e('Publish', 'wp-data-summary') ?></th>
-                <th><?php _e('Draft', 'wp-data-summary') ?></th>
+                <th><?php _e('Name', 'solid-data-summary') ?></th>
+                <th><?php _e('Label', 'solid-data-summary') ?></th>
+                <th><?php _e('Publish', 'solid-data-summary') ?></th>
+                <th><?php _e('Draft', 'solid-data-summary') ?></th>
             </tr>
         </thead>
         <tbody>
@@ -93,14 +93,14 @@ function the_taxonomies_table() {
         return $a['count'] > $b['count'] ? -1 : 1;
     });
     ?>
-    <h2><?php _e('Taxonomies', 'wp-data-summary') ?></h2>
+    <h2><?php _e('Taxonomies', 'solid-data-summary') ?></h2>
     <table>
         <thead>
             <tr>
-                <th><?php _e('Name', 'wp-data-summary') ?></th>
-                <th><?php _e('Label', 'wp-data-summary') ?></th>
-                <th><?php _e('Object Type', 'wp-data-summary') ?></th>
-                <th><?php _e('Count', 'wp-data-summary') ?></th>
+                <th><?php _e('Name', 'solid-data-summary') ?></th>
+                <th><?php _e('Label', 'solid-data-summary') ?></th>
+                <th><?php _e('Object Type', 'solid-data-summary') ?></th>
+                <th><?php _e('Count', 'solid-data-summary') ?></th>
             </tr>
         </thead>
         <tbody>
